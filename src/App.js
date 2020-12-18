67e9baa15f55711ff3570471f601d00e0a510775
jultimrova@gmail.com <splntrs019>
2020-12-18 19:33:03 +0200

    create search form, send request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import React, {useState, useEffect} from 'react'
 import './App.css';
+import Recipe from "./components/Recipe";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+const App = () => {
+    const API_ID = '48f158c7'
+    const API_KEY = 'fad331c93d981a7835793a463b765aa6'
+    const [recipes, setRecipes] = useState([])
+
+    useEffect(() => {
+        getRecipes()
+    }, [])
+
+    const getRecipes = async () => {
+        const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`)
+        const data = await response.json()
+        setRecipes(data.hits)
+        console.log(recipes)
+    }
+
+    return (
+        <div className="App">
+            <div className="search">
+                <form className='search__form'>
+                    <input className='search__bar' type="text"/>
+                    <button className='search__btn'
+                            type='submit'
+                            onClick={() => console.log('submit')}>Search
+                    </button>
+                </form>
+                <Recipe/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Recipe.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Recipe = () => {
+    return <>
+        <h3>Title</h3>
+        <p>Calories</p>
+        <img src="" alt="recipe"/>
+    </>
+}
+
+export default Recipe
